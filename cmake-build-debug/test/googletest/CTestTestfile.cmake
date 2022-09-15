07658a36369d783eee15ce75a17dab1ac733ffc4
andres <urizandres@estudiantec.cr>
2022-09-14 21:27:25 -0600

    final

---
+++ cmake-build-debug/test/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/andres/Documents/gtest/test/googletest
+# Build directory: /home/andres/Documents/gtest/cmake-build-debug/test/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
